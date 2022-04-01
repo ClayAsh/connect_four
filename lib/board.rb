3064283882512f8b0a8e7f89b6f4efb50d4ccede
philmarcu <philmark18@gmail.com>
2022-04-01 12:18:29 -0600

    Finish board display

--- lib/board.rb
+++ lib/board.rb
@@ -1,33 +1,37 @@
-
 class Board
-  attr_reader :board
+  attr_reader :board, :welcome_message, :generate_board
   def initialize
-    @board = [
-      ["A","B","C","D","E","F","G"], #0
-      [".",".",".",".",".",".","."], #1 Top row
-      [".",".",".",".",".",".","."], #2
-      [".",".",".",".",".",".","."],  #3
-      [".",".",".",".",".",".","."], #4
-      [".",".",".",".",".",".","."], #5
-      [".",".",".",".",".",".","."]  #6 Bottom row
-    ]
-
+    @board = {
+      A: [".", ".", ".", ".", ".", ".", "."],
+      B: [".", ".", ".", ".", ".", ".", "."],
+      C: [".", ".", ".", ".", ".", ".", "."],
+      D: [".", ".", ".", ".", ".", ".", "."],
+      E: [".", ".", ".", ".", ".", ".", "."],
+      F: [".", ".", ".", ".", ".", ".", "."],
+      G: [".", ".", ".", ".", ".", ".", "."]
+    }
   end
-
   def welcome_message
-    puts "Welcome to Connect Four!"
+    return "Welcome to Connect Four!"
   end
-
   def generate_board
-  board.each do |row|
-    row.each do|colomn|
-       colomn.to_s + " "
+    print_key = ""
+      board.each_key do |key|
+        print_key << key.to_s
       end
-      puts row.join("")
+        puts print_key
+      6.times do |index|
+      board.each_key do |value|
+      print board[value][index]
+    end
+      puts " "
     end
   end
 end
-
-board =  Board.new
-board.welcome_message
-board.generate_board
+  # def empty?
+  # end
+  # def full?
+  # end
+  board = Board.new
+  board.welcome_message
+  board.generate_board
