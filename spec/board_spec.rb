4e85fa8b72e9d478839cb2f9168971ea1439cf69
philmarcu <philmark18@gmail.com>
2022-04-01 12:18:53 -0600

    Pass board tests

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,9 +1,25 @@
-require 'rspec'
-require './lib/board'
+require "pry"
+require "rspec"
+require "./lib/board"
 
 RSpec.describe Board do
   it "exists" do
+  board = Board.new
+  expect(board).to be_an_instance_of(Board)
+  end
+  it "welcomes the player " do
     board = Board.new
-    expect(board).to be_an_instance_of(Board)
+  #---ALT----#
+    # OLD WELCOME METHOD ATTEMPTS
+    # welcome = message('welcome')
+    # board.welcome.message
+    # expect(welcome).to have_received(:message).with("welcome")
+    # board.welcome_message
+    expect(board.welcome_message).to eq("Welcome to Connect Four!")
+  end
+  it "generates the game board" do
+    board = Board.new
+    # binding.pry
+    expect(board.generate_board).to eq(6)
   end
 end
