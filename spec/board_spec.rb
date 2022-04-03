5580549bbf86958b2a330d5a27311db66a6220ab
philmarcu <philmark18@gmail.com>
2022-04-03 09:10:00 -0600

    Add 2 board tests to spec

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -6,4 +6,14 @@ RSpec.describe Board do
     board = Board.new
     expect(board).to be_an_instance_of(Board)
   end
+
+  it "welcomes the player " do
+    board = Board.new
+    expect(board.welcome_message).to eq("Welcome to Connect Four!")
+  end
+
+  it "generates the game board" do
+    board = Board.new
+    expect(board.generate_board).to eq(6)
+  end
 end
