4ce793e8ff7e5eca7084e1bb6cb1ab5adb1ba919
philmarcu <philmark18@gmail.com>
2022-04-04 17:30:20 -0600

    Resolve board merge conflict

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -5,7 +5,6 @@ require "./lib/board"
 RSpec.describe Board do
   it "exists" do
   board = Board.new
-
   expect(board).to be_an_instance_of(Board)
   end
 
@@ -18,7 +17,6 @@ RSpec.describe Board do
   it "generates the game board" do
     board = Board.new
     expect(board.generate_board).to eq(6)
-
   end
 end
 
