e2c54212ed415c321dfd6a3f33d6340a8bdd9dc3
philmarcu <philmark18@gmail.com>
2022-04-05 21:29:44 -0600

    Update column winner test

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -11,12 +11,32 @@ RSpec.describe Game do
 
   it "can declare a column winner" do
     game = Game.new
+    board = Board.new
+    player = Player.new
+
+    player.human_player("A")
+    player.human_player("A")
+    player.human_player("A")
+    player.human_player("A")
     game.column_winner
+
+    expect(game.column_winner).to eq(human_player)
+  end
+
+  it "can declare a horizontal_winner" do
+    game = Game.new
+    board = Board.new
+    player = Player.new
+
     expect(player.board)
   end
 
   it "can declare a diagonal winner" do
     game = Game.new
+    board = Board.new
+    player = Player.new
+
+
     expect(player.board)
   end
 end
