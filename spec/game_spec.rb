cdda59f97d4e8ded6f83fa47b431a11ce3a21146
philmarcu <philmark18@gmail.com>
2022-04-06 10:24:33 -0600

    Write out diagonal winner test

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -42,7 +42,28 @@ RSpec.describe Game do
     board = Board.new
     player = Player.new
 
+    player.human_player("D")
+    player.computer_player("C")
+    player.human_player("C")
+    player.computer_player("B")
+    player.human_player("A")
+    player.computer_player("B")
+    player.human_player("B")
+    player.computer_player("A")
+    player.human_player("A")
+    player.computer_player("E")
+    player.human_player("A")
 
-    expect()
+    expect(player.board.board).to eq({
+      :A=>[".", ".", "X", "X", "O", "X"],
+      :B=>[".", ".", ".", "X", "O", "O"],
+      :C=>[".", ".", ".", ".", "X", "O"],
+      :D=>[".", ".", ".", ".", ".", "X"],
+      :E=>[".", ".", ".", ".", ".", "O"],
+      :F=>[".", ".", ".", ".", ".", "."],
+      :G=>[".", ".", ".", ".", ".", "."]
+      })
+
+    expect(game.diagonal_winner).to eq(player.human_player)
   end
 end
